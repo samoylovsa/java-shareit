c0e70845224c5aba92ca482d9692dc7b95dbcae0
Sergey Samoylov <sam0lov@yandex.ru>
2025-07-30 20:33:44 +0300

    Add error handler

---
+++ src/main/java/ru/practicum/shareit/error/ErrorHandler.java
@@ -0,0 +1,19 @@
+package ru.practicum.shareit.error;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleException(Exception e) {
+        log.warn("Error", e);
+        return new ErrorResponse(e.getMessage());
+    }
+}
